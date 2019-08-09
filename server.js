28371dd3abd36e2625b00b4fec8931d62d4872ea
Joseph Cantos <joseph.cantos@hoegh.com>
2019-08-09 19:47:38 +0800

    added components and absolute pathing

--- server.js
+++ server.js
@@ -3,16 +3,27 @@ const path= require('path');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require('mongoose');
 
+//import routes
+const routes = require('./routes');
+app.use('/', routes)
 
-// to serve static assets
+//import models
+mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: true});
+
+//Middleware
+
+// to serve static assets. in the future we might add images and videos please create static locations
 app.use(express.static('views/app/build'));
 
 // express connection to react app
 app.get('*', function(req,res) {
     res.sendFile(path.join(__dirname, './views/app/public/index.html'));
-
 })
+
+
+
 
 app.listen(PORT, function(){
 
